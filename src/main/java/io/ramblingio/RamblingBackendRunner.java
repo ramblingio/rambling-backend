45f561b1e4e7c6e4a2e223140502dad72ab23208
Christopher Kolstad <christopher.kolstad@gmail.com>
2016-06-29 19:53:00 +0200

    Running app with health check

---
+++ src/main/java/io/ramblingio/RamblingBackendRunner.java
@@ -0,0 +1,9 @@
+package io.ramblingio;
+
+import org.springframework.boot.SpringApplication;
+
+public class RamblingBackendRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(RamblingConfig.class, args);
+    }
+}
---
+++ src/main/kotlin/io/ramblingio/RamblingConfig.kt
@@ -0,0 +1,11 @@
+package io.ramblingio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+open class RamblingConfig() {
+
+}
+
+
